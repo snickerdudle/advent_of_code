cac19c6a66434d461311d3c31814a6165c80823b
Harry Bleyan <3344438+snickerdudle@users.noreply.github.com>
2022-12-22 08:07:58 -0500

    Add blank day template

---
+++ make_new_day.py
@@ -0,0 +1,40 @@
+# Make a folder named "day_xx" where xx is the number of the day. In that
+# folder, make a file named "day_xx.txt" and paste the puzzle input into it.
+# Then, create a file named "day_xx.py" and paste template code into it.
+
+import sys
+import requests
+import os
+from pathlib import Path
+
+SESSION_ID = os.environ["AOC_SESSION_ID"]
+
+
+def getPuzzleForDayX(day: int):
+    # Go to the webpage and download the puzzle input into a variable
+    url = f"https://adventofcode.com/2022/day/{day}/input"
+    page_content = requests.get(url, cookies={"session": SESSION_ID}).text
+    return page_content
+
+
+if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        print("Usage: python make_new_day.py <day>")
+        sys.exit(1)
+
+    day = sys.argv[1]
+    page_content = getPuzzleForDayX(int(day))
+    cur_path = Path(__file__).resolve().parent
+    day_path = cur_path / f"day_{day}"
+    day_path.mkdir(exist_ok=True)
+
+    if not (day_path / f"day_{day}.txt").exists():
+        with open(day_path / f"day_{day}.txt", "w") as f:
+            f.write(page_content)
+        print('Created file "day_{day}.txt" with puzzle input.'.format(day=day))
+
+    if not (day_path / f"day_{day}.py").exists():
+        with open(day_path / f"day_{day}.py", "w") as f:
+            with open(cur_path / "blank.py", "r") as blank:
+                f.write(blank.read().replace("XXX", day))
+        print('Created file "day_{day}.py" with template code.'.format(day=day))
