855cb614a5543ee76780e45bdbc111e26e1561eb
Harry Bleyan <3344438+snickerdudle@users.noreply.github.com>
2023-05-29 18:52:19 -0700

    Day 21 Part 2

--- day_21/day_21.py
+++ day_21/day_21.py
@@ -1,5 +1,6 @@
 """Day 21."""
 from typing import Union, Optional, Type
+from functools import cache
 
 MonkeyType = Union[str, Type["Monkey"]]
 
@@ -24,33 +25,49 @@ class Monkey:
     ):
         self.name = name
         self._val = val
-        self._left = left
-        self._right = right
-        self._operation = operation
+        self.left = left
+        self.right = right
+        self.operation = operation
+
+    def get_left_obj(self):
+        if self.left is None:
+            return None
+        return all_monkeys[self.left]
+
+    def get_right_obj(self):
+        if self.right is None:
+            return None
+        return all_monkeys[self.right]
 
     @property
     def val(self):
-        print(self.name)
         if self._val is None:
             self.update_val()
         return self._val
 
     def update_val(self):
-        if self._left is None:
+        if self.left is None:
             raise ValueError(f"No value or left for {self.name}")
-        if self._right is None:
+        if self.right is None:
             raise ValueError(f"No value or right for {self.name}")
-        if self._operation is None:
+        if self.operation is None:
             raise ValueError(f"No operation for {self.name}")
-        if isinstance(self._operation, str):
-            self._operation = op_mapping[self._operation]
-        self._val = self._operation(
-            all_monkeys[self._left].val, all_monkeys[self._right].val
-        )
+        op = op_mapping[self.operation]
+        self._val = op(all_monkeys[self.left].val, all_monkeys[self.right].val)
         return self._val
 
+    @cache
+    def is_human_branch(self):
+        if self.name == "humn":
+            return True
+        if self.left is not None and self.get_left_obj().is_human_branch():
+            return True
+        if self.right is not None and self.get_right_obj().is_human_branch():
+            return True
+        return False
 
-def get_data():
+
+def get_data_part_1():
     data = open("day_21.txt").read().strip().split("\n")
 
     for name, content in [line.split(": ") for line in data]:
@@ -65,13 +82,58 @@ def get_data():
 
 
 def part_1():
-    data = get_data()
-    print(data)
-    print(data["root"].val)
+    get_data_part_1()
+    print(all_monkeys["root"].val)
 
 
 def part_2():
-    pass
+    human = all_monkeys["humn"]
+    cur_root = all_monkeys["root"]
+
+    human_branch, non_human_branch = (
+        cur_root.get_left_obj(),
+        cur_root.get_right_obj(),
+    )
+    if human_branch is None or non_human_branch is None:
+        raise ValueError("No branches")
+    if non_human_branch.is_human_branch():
+        human_branch, non_human_branch = non_human_branch, human_branch
+
+    target_val = non_human_branch.val
+    cur_root = human_branch
+
+    invert_operations = {
+        "-": int.__add__,
+        "+": int.__sub__,
+        "*": int.__floordiv__,
+        "/": int.__mul__,
+    }
+
+    while human_branch is not None:
+        print(cur_root.name, target_val)
+        # Find the next human branch, and invert the opetations that led to
+        # the current target_val
+        flipped = False
+        human_branch, non_human_branch = (
+            cur_root.get_left_obj(),
+            cur_root.get_right_obj(),
+        )
+        if human_branch is None or non_human_branch is None:
+            break
+        if non_human_branch.is_human_branch():
+            human_branch, non_human_branch = non_human_branch, human_branch
+            flipped = True
+
+        if flipped and cur_root.operation == "-":
+            target_val = int.__sub__(non_human_branch.val, target_val)
+        elif flipped and cur_root.operation == "/":
+            target_val = int.__floordiv__(non_human_branch.val, target_val)
+        else:
+            inv_op = invert_operations[cur_root.operation]
+            target_val = inv_op(target_val, non_human_branch.val)
+        cur_root = human_branch
+
+    print(target_val)
 
 
 if __name__ == "__main__":
